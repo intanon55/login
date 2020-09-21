c4ed248d81032b5f7127db078e81b1a152da8378
intanon55 <53069180+intanon55@users.noreply.github.com>
2020-09-21 20:11:59 +0700

    Update config.php

--- config.php
+++ config.php
@@ -2,10 +2,10 @@
 class Database
 {   
      //ตั้งค่าเชื่อมต่อ DATABASE
-    private $host = "localhost";
-    private $db_name = "testpdo";
-    private $username = "root";
-    private $password = "";
+    private $host = "us-cdbr-east-02.cleardb.com";
+    private $db_name = "heroku_b17c7c5f045d5cd";
+    private $username = "bd70755a0d98e0";
+    private $password = "0b0e9437";
     public $conn;
      
     public function dbConnection()
